eb0282e1babd62d5810cbb563010e6f8b020c5cd
hongwon <hongwon1031@naver.com>
2025-03-04 23:36:43 +0900

    테스트

--- 테스트/test.py
+++ 테스트/test.py
@@ -1,5 +1,18 @@
-print(input())
-print(input())
-print(input())
-print(input())
-a,b,c,d = map(int,(input() for _ in range(4)))
+import sys
+n = sys.stdin.readline()
+
+
+for i in range(int(n)):
+    a = sys.stdin.readline().rstrip()
+    a,b = a.split(' ')
+    print(int(a)+int(b))
+
+
+
+
+
+'''1 1
+12 34
+5 500
+40 60
+1000 1000'''
