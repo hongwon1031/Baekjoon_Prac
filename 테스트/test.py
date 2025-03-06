a0b6b2c172ec41234a51955f423de3b7a07347d3
hongwon <hongwon1031@naver.com>
2025-03-06 11:15:07 +0900

    아졸령

--- 테스트/test.py
+++ 테스트/test.py
@@ -1,14 +1,5 @@
-'''a = input()
-count = a.count(' ')
-if (a[0] == ' ')or (a[-1] == ' '):
-    count -= 1
-
-print(count+1)
-
-'''
-a = input()
-count = 0
-for i in range(1,len(a)):
-    if a[i] == ' ':
-        count += 1
-print(count)
+alpha = 'abcdefghijklnmopqrstuvwxyz'
+print(len(alpha))
+n = input()
+for i in range(n):
+    
