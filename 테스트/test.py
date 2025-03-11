86ce7283a6bc8e3dee63e0bfd0a656f13b461e30
hongwon <hongwon1031@naver.com>
2025-03-11 20:05:20 +0900

    허허

--- 테스트/test.py
+++ 테스트/test.py
@@ -1,4 +1,19 @@
 import sys
 def input():
     return sys.stdin.readline()
+n,b = map(str,input().split())
+print(n,b)
+dict = {}
+for i in range(1,10):
+    dict[i] = i
+'''for i in range(10,36):
     
+sum = 0
+for i in range(len(n)-1,-1,-1):
+    j = 0
+    sum += (n[i]**j) * int(b)
+    print(sum)
+    print('j=',j)
+    j += 1
+print(sum)'''
+print(ord('Z'))
