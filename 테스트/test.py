13bb285dfc1d169a8f9ccabaec872e68ad511507
hongwon <hongwon1031@naver.com>
2025-03-13 00:40:01 +0900

    시간복잡도 아오

--- 테스트/test.py
+++ 테스트/test.py
@@ -1,16 +1,14 @@
-li = [list(map(str,input().split())) for _ in range(20)]
-grade = {'A+':4.5,'A0':4.0,
-        'B+':3.5,'B0':3.0,
-        'C+':2.5,'C0':2.0,
-        'D+':1.5,'D0':1.0,'F':0.0}
+import sys
+from collections import deque
+def input():
+    return sys.stdin.readline()
 
-sum_1 = 0
-sum_2 = 0
-for i in range(len(li)):
-    if li[i][2] == 'P':
-        continue
-    sum_1 += float(grade[li[i][2]])*float(li[i][1])
-    sum_2 += float(li[i][1])
-
-
-print(sum_1/sum_2)
+N = int(input())
+a = list(map(int,input().split()))
+M = int(input())
+b = list(map(int,input().split()))
+for i in range(M):
+    if b[i] in a:
+        print(1,end=' ')
+    else:
+        print(0,end=' ')
