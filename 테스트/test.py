706e705bae33a481c3e97b2fcf30a853026371c6
hongwon <hongwon1031@naver.com>
2025-03-12 22:21:23 +0900

    허허

--- 테스트/test.py
+++ 테스트/test.py
@@ -1,19 +1,16 @@
-import sys
-def input():
-    return sys.stdin.readline()
-n,b = map(str,input().split())
-print(n,b)
-dict = {}
-for i in range(1,10):
-    dict[i] = i
-'''for i in range(10,36):
-    
-sum = 0
-for i in range(len(n)-1,-1,-1):
-    j = 0
-    sum += (n[i]**j) * int(b)
-    print(sum)
-    print('j=',j)
-    j += 1
-print(sum)'''
-print(ord('Z'))
+li = [list(map(str,input().split())) for _ in range(20)]
+grade = {'A+':4.5,'A0':4.0,
+        'B+':3.5,'B0':3.0,
+        'C+':2.5,'C0':2.0,
+        'D+':1.5,'D0':1.0,'F':0.0}
+
+sum_1 = 0
+sum_2 = 0
+for i in range(len(li)):
+    if li[i][2] == 'P':
+        continue
+    sum_1 += float(grade[li[i][2]])*float(li[i][1])
+    sum_2 += float(li[i][1])
+
+
+print(sum_1/sum_2)
