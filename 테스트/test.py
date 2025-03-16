b4a7ae4a3611325d30412c5031d1631039d1a890
hongwon <hongwon1031@naver.com>
2025-03-16 22:47:39 +0900

    gg

--- 테스트/test.py
+++ 테스트/test.py
@@ -1,14 +1,18 @@
 import sys
-from collections import deque
 def input():
     return sys.stdin.readline()
+M,N = map(int,input().split())
 
-N = int(input())
-a = list(map(int,input().split()))
-M = int(input())
-b = list(map(int,input().split()))
-for i in range(M):
-    if b[i] in a:
-        print(1,end=' ')
-    else:
-        print(0,end=' ')
+li = [i for i in range(2,M+1)]
+p = min(li)
+print(li,p)
+for _ in range(len(li)):
+    for i in range(len(li)):
+        print('i =',i)
+        print('li =',li)
+        if li[i]%p == 0:
+            li.remove(li[i])
+            print(li[i])
+            print(i,'전')
+            i -= 1
+            print(i,'후')
