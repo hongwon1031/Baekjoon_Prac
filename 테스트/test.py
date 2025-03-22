b9ff7dead11e954fe27f89073eb7d84f59381f5d
hongwon <hongwon1031@naver.com>
2025-03-22 23:29:58 +0900

    ㅎㅎ

--- 테스트/test.py
+++ 테스트/test.py
@@ -1,18 +1,5 @@
 import sys
-
-def gcd(a,b):   #최대공약수
-    while b>0:
-        a,b = b,a%b
-    return a
-
 def input():
     return sys.stdin.readline()
 
-a,b = map(int,input().split())
-def old_gcd(a,b):
-    count = 0
-    while a!=b:
-        a, b = max(a,b) - min(a,b), min(a,b)
-        count += 1
-    return count
-print(old_gcd(a,b))
+n,h,v = map(int,input().split())
