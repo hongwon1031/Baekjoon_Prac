4f12ad4350d0a0475c950c7a88cc40432a1608e6
hongwon <hongwon1031@naver.com>
2025-03-24 09:13:27 +0900

    허허

--- 테스트/test.py
+++ 테스트/test.py
@@ -1,5 +1,20 @@
 import sys
+
 def input():
     return sys.stdin.readline()
 
-n,h,v = map(int,input().split())
+count = 0
+n = int(input())
+dp = [-1] + [0,1,1] + [-1]*(n-3)
+
+
+for i in range(4,n+1):
+    if i % 3 == 0 and i % 2 == 0:
+        dp[i] = min(dp[i-1],dp[i//2],dp[i//3]) + 1
+    elif i % 2 == 0:
+        dp[i] = min(dp[i-1],dp[i//2]) + 1
+    elif i % 3 == 0:
+        dp[i] = min(dp[i-1],dp[i//3]) + 1
+    else:
+        dp[i] = dp[i-1] + 1
+print(dp[n])
