918b1e85f2a525139b56a4ae7a025b2633b42915
hongwon1031 <hongwon1031@naver.com>
2025-04-02 21:05:02 +0900

    ㅋㅋ

--- 테스트/test.py
+++ 테스트/test.py
@@ -1,20 +1,7 @@
 import sys
-
 def input():
     return sys.stdin.readline()
 
-count = 0
-n = int(input())
-dp = [-1] + [0,1,1] + [-1]*(n-3)
-
-
-for i in range(4,n+1):
-    if i % 3 == 0 and i % 2 == 0:
-        dp[i] = min(dp[i-1],dp[i//2],dp[i//3]) + 1
-    elif i % 2 == 0:
-        dp[i] = min(dp[i-1],dp[i//2]) + 1
-    elif i % 3 == 0:
-        dp[i] = min(dp[i-1],dp[i//3]) + 1
-    else:
-        dp[i] = dp[i-1] + 1
-print(dp[n])
+li = [int(input()) for _ in range(10)]
+sum = li[0] - sum(li[1:])
+print(sum)
